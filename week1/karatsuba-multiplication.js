af1b3d08d6d1743ece8f2692d72f4293369578d7
edraitsev <ddrrai@gmail.com>
2019-02-01 00:08:13 +0100

    Added real BigInt-less example

--- week1/karatsuba-multiplication.js
+++ week1/karatsuba-multiplication.js
@@ -1,27 +1,3 @@
-const pow10 = (multiplicand, n) => String(multiplicand) + ''.padEnd(n, 0)
-
-// I stole this function from stackoverflow
-const sum = (a, b) => {
-  const aa = Array.from(String(a), Number)
-  const bb = Array.from(String(b), Number)
-  const result = []
-  let carry = 0
-  let i = Math.max(aa.length, bb.length)
-
-  while (i--) {
-    carry += (aa.pop() || 0) + (bb.pop() || 0)
-    result.unshift(carry % 10)
-    carry = Math.floor(carry / 10)
-  }
-  while (carry) {
-    result.unshift(carry % 10)
-    carry = Math.floor(carry / 10)
-  }
-  return result.join('')
-}
-
-const substruct = (a, b) => String(BigInt(a) - BigInt(b))
-
 const multiply = (multiplicand1, multiplicand2) => {
   const string1 = String(multiplicand1)
   const string2 = String(multiplicand2)
@@ -47,6 +23,84 @@ const multiply = (multiplicand1, multiplicand2) => {
     + (BigInt(10) ** BigInt(Math.ceil(n / 2))) * (abcd - ac - bd) + bd
 }
 
+
+// /////////////////////////////////
+// Strings only version starts here
+// /////////////////////////////////
+
+const pow10 = (multiplicand, n) => String(multiplicand) + ''.padEnd(n, 0)
+
+// I stole this function from codewars
+function compare(x, y) {
+  const a = x.replace(/^0+/, '')
+  const b = y.replace(/^0+/, '')
+  if (a.length > b.length) return 1
+  if (a.length < b.length) return -1
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) return 1
+    if (a[i] < b[i]) return -1
+  }
+  return 0
+}
+
+// I stole this function from codewars
+function sum(numA, numB) {
+  let a = String(numA)
+  let b = String(numB)
+  const s1 = a[0] === '-'
+  const s2 = b[0] === '-'
+  a = a.replace(/^-/, '')
+  b = b.replace(/^-/, '')
+  if (s1 && s2) return `-${sum(a, b)}`
+  // eslint-disable-next-line no-use-before-define
+  if (s1 && !s2) return subtract(b, a)
+  // eslint-disable-next-line no-use-before-define
+  if (!s1 && s2) return subtract(a, b)
+  let res = ''; let
+    c = 0
+  a = a.split('')
+  b = b.split('')
+  while (a.length || b.length || c) {
+    // eslint-disable-next-line no-bitwise
+    c += ~~a.pop() + ~~b.pop()
+    res = (c % 10) + res
+    c = c > 9
+  }
+  return res.replace(/^0+/, '') || '0'
+}
+
+// I stole this function from codewars
+function subtract(numA, numB) {
+  let a = String(numA)
+  let b = String(numB)
+  const s1 = a[0] === '-'
+  const s2 = b[0] === '-'
+  a = a.replace(/^-/, '').replace(/^0+/, '')
+  b = b.replace(/^-/, '').replace(/^0+/, '')
+  if (s1 + s2 === 1) return (s1 ? '-' : '') + sum(a, b)
+  const aa = a
+  const bb = b
+  if (compare(aa, bb) === -1) {
+    [a, b] = [b, a]
+  }
+  a = [...a]
+  b = [...b]
+  for (let i = a.length - 1; i >= 0; i--) {
+    const d = b[b.length - a.length + i] || 0
+    // eslint-disable-next-line operator-assignment
+    a[i] = a[i] - d
+    if (a[i] < 0) {
+      a[i] += 10
+      a[i - 1]--
+    }
+  }
+  a = a.join('').replace(/^0+/, '')
+  if ((s1 && compare(aa, bb) === 1) || (!s1 && compare(aa, bb) === -1)) {
+    a = `-${a}`
+  }
+  return a || '0'
+}
+
 const stringMultiply = (multiplicand1, multiplicand2) => {
   const string1 = String(multiplicand1)
   const string2 = String(multiplicand2)
@@ -64,11 +118,24 @@ const stringMultiply = (multiplicand1, multiplicand2) => {
   const c = string2.padStart(n, 0).slice(0, n / 2)
   const d = string2.padStart(n, 0).slice(n / 2)
 
-  const ac = multiply(a, c)
-  const bd = multiply(b, d)
-  const abcd = multiply(sum(a, b), sum(c, d))
+  const ac = stringMultiply(a, c)
+  const bd = stringMultiply(b, d)
+  const abcd = stringMultiply(sum(a, b), sum(c, d))
 
-  return sum(sum(pow10(ac, n), pow10(substruct(substruct(abcd, ac), bd), Math.ceil(n / 2))), bd)
+  // how to properly format this shit?
+  return sum(
+    sum(
+      pow10(ac, n),
+      pow10(
+        subtract(
+          subtract(abcd, ac),
+          bd,
+        ),
+        Math.ceil(n / 2),
+      ),
+    ),
+    bd,
+  )
 }
 
 
